27356bb683dd7ba274d02e3f98bc936c28e60306
paynejones <paynejones01@gmail.com>
2026-04-08 12:16:13 -0700

    Update resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header(){
             <div className={styles.links}>
                 <a className={styles.link} href="https://github.com/paynejones" aria-label="My Github Account"><FontAwesomeIcon icon={faGithub} size="xl"/></a>
                 <a className={styles.link} href="https://www.linkedin.com/in/payne-jones/" aria-label="My LinkedIn Account"><FontAwesomeIcon icon={faLinkedin} size="xl" transform="grow-1"/></a>
-                <a className={styles.link} href="/Payne_Jones_Updated_Resume_July_2025.pdf" download="Payne_Jones_Resume.pdf" aria-label="My Resume"><FontAwesomeIcon icon={faUser} size="xl" transform="shrink-7 down-1.5" mask={faFile}/></a>
+                <a className={styles.link} href="/Payne_Jones_Resume_Apr_2026.pdf" download="Payne_Jones_Resume.pdf" aria-label="My Resume"><FontAwesomeIcon icon={faUser} size="xl" transform="shrink-7 down-1.5" mask={faFile}/></a>
             </div>
             <div className={styles.centername}>
                 Payne Jones
